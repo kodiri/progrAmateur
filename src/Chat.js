ee256286b98a490384707fd82800cc3a582dcdc5
Keivon <keivonproctor@hotmail.com>
2019-09-09 10:02:43 +0100

    #First

---
+++ src/Chat.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Chat() {
+    return (
+        <div>
+            <label>Chat</label>
+            <textArea></textArea>
+        </div>
+    );
+}
