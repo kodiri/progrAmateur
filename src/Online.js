6bb854f94e2fc003c23d5bbbf947ae7fcfc0891f
Elopfer <lopezesteban8@gmail.com>
2019-09-13 12:02:00 +0100

    Add css for the chat, messages and online component

--- src/Online.js
+++ src/Online.js
@@ -2,6 +2,9 @@ import React from "react";
 
 export default function Online(props) {
     return ( 
-        props.data.map(m => <li id={m[0]}>{m[1]}</li>)
+    props.data.map(m => {
+        console.log('m0:', m[0], 'm1:', m[1]);
+    return <li className='users' id={m[0]}>{m[1]}</li>
+    })
     );
 }
